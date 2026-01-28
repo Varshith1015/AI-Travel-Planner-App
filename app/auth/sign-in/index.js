b4c6b930ef8626cdcbb7d9a902193faa6f1ad966
Varshith <varshithravipati2003@gmail.com>
2026-01-28 22:37:26 +0530

    Progress: expo setup,Application font, Built landing page,sign in and signup pages

---
+++ app/auth/sign-in/index.js
@@ -0,0 +1,120 @@
+import { View, Text, TextInput,StyleSheet,TouchableOpacity} from 'react-native'
+import React ,{useEffect} from 'react'
+import {useNavigation} from "expo-router"
+import {Colors} from  "./../../../constants/Colours"
+import { useRouter } from 'expo-router'
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function SignIn() {
+    const navigation = useNavigation()
+    const router=useRouter(); 
+    useEffect(()=>{
+        navigation.setOptions({
+            headerShown:false
+        })
+    },[])
+
+  return (
+    <View style={{
+        padding:25,
+        paddingTop:40,
+        height:'100%',    
+        backgroundColor:Colors.WHITE
+    }}>
+        <TouchableOpacity onPress={()=>router.back()}>
+            <Ionicons name="arrow-back" size={24} color="black" />
+        </TouchableOpacity>
+        
+        <Text style={{
+            fontFamily:'outfit-bold',
+            fontSize:30,
+            marginTop:30
+        }}>
+            Let's Sign You In
+        </Text>
+        <Text style={{
+            fontFamily:'outfit',
+            fontSize:30,
+            color:Colors.GRAY,
+            marginTop:20
+        }}>
+            Welcome Back
+        </Text>
+        <Text style={{
+            fontFamily:'outfit',
+            fontSize:20,
+            color:Colors.GRAY,
+            marginTop:10
+        }}>
+            You have been missed!
+        </Text>
+
+
+        {/* Email */}
+        <View style={{
+            marginTop:50
+        }}>
+            <Text style={{
+                fontFamily:'outfit',
+
+            }}> Email </Text>
+            <TextInput  style={styles.input} placeholder='enter Email'></TextInput>
+        </View>
+
+
+
+        {/* //password */}
+        <View style={{
+            marginTop:20
+        }}>
+            <Text style={{
+                fontFamily:'outfit',
+
+            }}> Password </Text>
+            <TextInput secureTextEntry={true} style={styles.input} placeholder='enter password'></TextInput>
+        </View>
+
+
+        {/* Sign In Button */}
+        <View style={{
+            padding:20,
+            backgroundColor:Colors.PRIMARY,
+            borderRadius:15,
+            marginTop:50
+        }}>
+            <Text style={{
+                color:Colors.WHITE,
+                textAlign:'center'
+            }}>Sign In</Text>
+        </View>
+
+        {/* Create account Button */}
+        <TouchableOpacity 
+        onPress={()=>router.replace('auth/sign-up')}
+            style={{
+            padding:20,
+            backgroundColor:Colors.WHITE,
+            borderRadius:15,
+            marginTop:20,
+            borderWidth:1
+        }}>
+            <Text style={{
+                color:Colors.PRIMARY,
+                textAlign:'center'
+            }}>Create Account</Text>
+            
+        </TouchableOpacity>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    input:{
+        padding:15,
+        borderWidth:1,
+        borderRadius:15,
+        borderColor:Colors.GRAY,
+        fontFamily:'outfit',
+    }
+})
---
+++ components/Login.jsx
@@ -0,0 +1,67 @@
+import { View, Text ,Image,StyleSheet, TouchableOpacity} from 'react-native'
+import React from 'react'
+import { Colors } from '@/constants/Colours'
+import { useRouter } from 'expo-router'
+
+export default function Login() {
+
+    const router=useRouter();
+
+  return (
+    <View>
+      <Image source={require('./../assets/images/login.jpg')}
+       style={{
+        width:'100%',
+        height:500
+       }}/>
+       <View style={styles.container}>
+        <Text style={{
+            fontSize:28,
+            fontFamily:'outfit-bold',
+            textAlign:'center',
+            
+        }}>
+            AI Travel Planner
+        </Text> 
+
+        <Text style={{
+            fontSize:17,
+            fontFamily:'outfit',
+            textAlign:'center',
+            color:Colors.GRAY,
+            marginTop:25
+        }}>
+            Discover your next adventure effortlessly. Personalized itineraries at your fingertips.Travel smarter with AI-driven insights.
+        </Text>
+        <TouchableOpacity style={styles.button} onPress={()=>router.push('auth/sign-in')}>
+            
+            <Text style={{
+                color:Colors.WHITE,
+                textAlign:'center',
+                fontFamily:'outfit',
+                fontSize:17
+            }}>Get Started</Text>
+        </TouchableOpacity>
+
+       </View>
+    </View>
+  )
+}
+
+//external styling
+const styles=StyleSheet.create({
+    container:{
+        backgroundColor:Colors.WHITE,
+        marginTop:-20,
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30,
+        padding:15,
+        height:'100%'
+    },
+    button:{
+        padding:15,
+        backgroundColor:Colors.PRIMARY,
+        borderRadius:99,
+        marginTop:'20%',
+    }
+})
